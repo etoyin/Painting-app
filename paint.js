6f5f5876622d3947f6797b598270f5743de49116
Adesina Toyin <toyinadesina60@gmail.com>
2015-07-14 16:55:51 +0100

    modified

--- paint.js
+++ paint.js
@@ -1,14 +1,16 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
-//my drawing line will be in 5px
+//an object to used to store the info of the drawing
+rect = {};
+drag = false;
+//my drawing line will be in 1px
 
 context.lineWidth = 1;
 //this variable will be used to detect whether the mouse button is pressed or not
-var down = false;
+ down = false;
 
 
 
-//e means events 
 //offsetLeft returns the number of pixels that are on the left
 //left side of the element.
 //clientX returns the X coordinates of the mouse pointer.
@@ -17,16 +19,17 @@ var down = false;
 //on th canvas
 var draw = function(e){
 
-	xPos = e.clientX - canvas.offsetLeft;
-	yPos = e.clientY - canvas.offsetTop;
+ 
+	xPos = e.pageX - this.offsetLeft;
+	yPos = e.pageY - this.offsetTop;
 
 	if(down == true){
 
 		context.lineTo(xPos, yPos);
 		context.stroke();
-	}
+	};
 
-}
+};
 
 
 
@@ -36,22 +39,24 @@ var draw = function(e){
 canvas.addEventListener('mousemove', draw);
 
 canvas.addEventListener('mouseup', function(){
-	down = false
-});
+	down = false;
+}, false);
 //when mouse is down, this will run
 canvas.addEventListener('mousedown', function(){
-
+ 
+      
 	down = true;
 	context.beginPath();
 	context.moveTo(xPos, yPos);
 	canvas.addEventListener('mousemove', draw);
 
-});
+}, false);
 
 
 
 var changeColor = function(color){
 	context.strokeStyle = color;
+	context.fillStyle = color;
 }
 
 var clearCanvas = function(){
@@ -59,6 +64,51 @@ var clearCanvas = function(){
 }
 
 var changeThickness = function(x){
-context.lineWidth = x;
-
+	context.lineWidth = x
 }
+
+
+var fillCanvas = function(){
+	context.fillRect(0,0,canvas.width,canvas.height);
+
+};
+
+var initRect = function() {
+  canvas.addEventListener('mousedown', mousedown);
+  canvas.addEventListener('mousemove', mousemove);
+  canvas.addEventListener('mouseup', mouseup);
+
+};
+var mousedown = function(e) {
+  rect.startX = e.pageX - this.offsetLeft;
+  rect.startY = e.pageY- this.offsetTop;
+  drag = true;
+}
+
+
+var mouseup = function(){
+  drag = false;
+};
+
+
+var mousemove = function(e){
+  if (drag == true){
+    rect.w = (e.pageX  - this.offsetLeft) - rect.startX;
+    rect.h = (e.pageY - this.offsetTop) - rect.startY ;
+    //context.clearRect(0,0,canvas.width,canvas.height);
+    drawRect();
+  };
+};
+
+
+var drawRect = function() {
+  context.fillRect(rect.startX, rect.startY, rect.w, rect.h);
+};
+
+
+
+
+
+
+
+
